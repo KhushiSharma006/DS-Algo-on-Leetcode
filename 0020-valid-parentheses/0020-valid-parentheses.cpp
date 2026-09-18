056846f8b491b73282a617e250cb30668294389c
Khushi Sharma <khushisharma.ks656@gmail.com>
2026-09-18 07:02:49 +0530

    Time: 0 ms (100%), Space: 9.3 MB (11.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
+
         stack<char>st;
+        unordered_map<char, char>pair{{')','('},{'}','{'},{']','['}};
+
+        for(char c: s){
+            if(c == '(' || c == '{' || c == '[') st.push(c);
+            else{
+                if(st.empty() || st.top() != pair[c]){
+                    return false;
+                }
+                st.pop();
+            }
+         
+          
+        }
+         return st.empty();
+        
+
+
+
+
+
+        /* stack<char>st;
         for(auto it:s){
             if(it == '(' || it == '{' || it == '['){
                 st.push(it);
@@ -18,7 +40,7 @@ public:
                 else return false;       
             }
         }
-        return st.empty();
+        return st.empty();*/
         
     }
 };
